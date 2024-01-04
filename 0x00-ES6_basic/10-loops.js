8bf1940b9ffa63aecc7b81a6563314ea111bac62
BABABUSOLA <queenbusola1@gmail.com>
2024-01-04 00:20:26 +0000

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const value = [];
-  for (const idx in array) {
-    value.push(`${appendString}${idx}`);
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
   }
 
-  return array;
+  return arrayEnd;
 }
